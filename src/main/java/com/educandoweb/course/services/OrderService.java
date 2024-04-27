1c3df7c7d19476e25e90d2b24ca2785e12d78a00
miguelmdemacedo@gmail.com <152532159+MiguelMdeMacedo@users.noreply.github.com>
2024-04-27 12:43:06 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	@GetMapping
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
